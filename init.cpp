3c0ed07b2c9a0434deaf1288e9fe98e908b5ed0a
FeldmeierMichael <102790823+FeldmeierMichael@users.noreply.github.com>
2025-12-04 09:24:42 +0100

    💬 Added Command Usage Message

--- init.cpp
+++ init.cpp
@@ -9,6 +9,7 @@
 
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
+#include "sat/bmc/bmc.h"
 
 #include <iostream>
 
@@ -18,14 +19,70 @@ namespace
 // Very simple ABC command: prints a greeting and its command line argumentss
 int Pexact_Command( Abc_Frame_t * pAbc, int argc, char ** argv )
 {
-    std::cout << "Hello world!" << std::endl;
-
-    for ( int i = 0; i < argc; i++ )
+    int c;
+    Bmc_EsPar_t Pars, *pPars = &Pars;
+    Bmc_EsParSetDefault( pPars );
+    Extra_UtilGetoptReset();
+    while ( ( c = Extra_UtilGetopt( argc, argv, "IN" ) ) != EOF )
     {
-        std::cout << "  argv[" << i << "]: " << argv[i] << std::endl;
+        switch ( c )
+        {
+        case 'I':
+            if ( globalUtilOptind >= argc )
+            {
+                Abc_Print( -1, "Command line switch \"-I\" should be followed by an integer.\n" );
+                goto usage;
+            }
+            pPars->nVars = atoi( argv[globalUtilOptind] );
+            globalUtilOptind++;
+            if ( pPars->nVars < 0 )
+                goto usage;
+            break;
+        case 'N':
+            if ( globalUtilOptind >= argc )
+            {
+                Abc_Print( -1, "Command line switch \"-N\" should be followed by an integer.\n" );
+                goto usage;
+            }
+            pPars->nNodes = atoi( argv[globalUtilOptind] );
+            globalUtilOptind++;
+            if ( pPars->nNodes < 0 )
+                goto usage;
+            break;
+        case 'h':
+            goto usage;
+        default:
+            goto usage;
+        }
+    }
+    if ( pPars->pTtStr == NULL )
+    {
+        Abc_Print( -1, "Truth table should be given on the command line.\n" );
+        return 1;
+    }
+    if ( pPars->nVars >= 2 && ( 1 << ( pPars->nVars - 2 ) ) != ( int )strlen( pPars->pTtStr ) )
+    {
+        Abc_Print( -1, "Truth table is expected to have %d hex digits (instead of %d).\n", ( 1 << ( pPars->nVars - 2 ) ), strlen( pPars->pTtStr ) );
+        return 1;
+    }
+    if ( pPars->nVars > pPars->nNodes * ( 2 - 1 ) + 1 )
+    {
+        Abc_Print( -1, "Function with %d variables cannot be implemented with %d two-input gates.\n", pPars->nVars, pPars->nNodes );
+        return 1;
+    }
+    if ( pPars->nVars > 4 )
+    {
+        Abc_Print( -1, "Function should not have more than 4 inputs.\n" );
+        return 1;
     }
 
-    return 0;
+
+usage:
+    Abc_Print( -2, "usage: twoexact [-IN] <hex>\n" );
+    Abc_Print( -2, "\t           exact synthesis of multi-input function using two-input gates\n" );
+    Abc_Print( -2, "\t-I <num> : the number of input variables [default = %d]\n", pPars->nVars );
+    Abc_Print( -2, "\t-N <num> : the number of two-input nodes [default = %d]\n", pPars->nNodes );
+    return 1;
 }
 
 // called during ABC startup
