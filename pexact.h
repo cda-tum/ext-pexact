9efe45ee363f15946d1ced2f4d4c0cbee9eeaf29
FeldmeierMichael <102790823+FeldmeierMichael@users.noreply.github.com>
2026-02-16 11:56:47 +0100

    :sparkles: Add Power CNF Encoding (#11)
    * ✨Introducing Power Constraints
    
    * 💬Added Comments
    
    * 🔨Clang-Tidy Work Nr.1
    
    * 🔨Clang-Tidy Work Nr.2
    
    * 🔨Clang-Tidy Work Nr.3
    
    * 🔨Clang-Tidy Work Nr.4
    
    * 🔨Clang-Tidy Work Nr.5
    
    * 🔨Clang-Tidy Work Nr.6
    
    * 🔨Clang-Tidy Work Nr.7
    
    * 🔨Clang-Tidy Work Nr.8
    
    * ✏️Added AI Feedback Nr.1
    
    * 🔨Clang-Tidy Work Nr.9
    
    * 🔨Clang-Tidy Work Nr.10
    
    * ✏️Added AI Feedback Nr.2
    
    * ✏️Added AI Feedback Nr.3
    
    * ✏️Added AI Feedback Nr.4
    
    * ✏️Added AI Feedback Nr.5
    
    * ✏️Fixed Docstrings
    
    * ✏️Added AI Feedback Nr.6
    
    * 🔨Clang-Tidy Work Nr.11
    
    * ✏️Added AI Feedback Nr.7
    
    * ✏️Added AI Feedback Nr.8
    
    * ✏️Added AI Feedback Nr.9
    
    * ✏️Added AI Feedback Nr.10
    
    * 🔨Clang-Tidy Work Nr.12
    
    * 🔨Clang-Tidy Work Nr.13
    
    * ✏️Added AI Feedback Nr.11
    
    * ✏️Added AI Feedback Nr.12
    
    * ✏️Added AI Feedback Nr.13
    
    * 🔨Clang-Tidy Work Nr.14
    
    * ✏️Added AI Feedback Nr.14
    
    * ✏️Added AI Feedback Nr.15
    
    * ✏️Added AI Feedback Nr.16
    
    * 👷 Including Review Feedback Nr.1
    
    * 🚨 Fixed Ubuntu Integration Fail Nr.1
    
    * ✏️Added AI Feedback Nr.17
    
    * 👷 Including Review Feedback Nr.2
    
    * ✏️Added AI Feedback Nr.18
    
    * 🔨Clang-Tidy Work Nr.15
    
    * 🔨Clang-Tidy Work Nr.16
    
    * :art: Small consistency fixes.
    
    * ✏️Added AI Feedback Nr.19
    
    * ✏️Added AI Feedback Nr.20
    
    * ✏️Added AI Feedback Nr.21
    
    ---------
    
    Co-authored-by: Marcel Walter <marcel.walter@tum.de>

--- pexact.h
+++ pexact.h
@@ -16,52 +16,98 @@
 #include "sat/bsat/satStore.h"
 #include "sat/cnf/cnf.h"
 
+#include <limits.h>
+
 
 #define MAJ_NOBJS 32  // Const0 + Const1 + nVars + nNodes
+
+const long long PEXACT_LLONG_MAX = LLONG_MAX;
+
 const int CONST_ZERO = 0;
 const int CONST_ONE = 1;
 const int CONST_TWO = 2;
 const int CONST_THREE = 3;
 const int CONST_FOUR = 4;
+const int CONST_FIVE = 5;
 const int CONST_SIX = 6;
+const int CONST_SEVEN = 7;
 const int CONST_TEN = 10;
 
+const int CONST_NINETY_SIX = 96;
+const int CONST_FIFTY_SIX = 56;
+
+
+typedef struct PexaMan_t_ PexaMan_t;
 /**
  * @brief pexact struct.
  *
  * @details
  * Containing SAT solver, starting index of variables and parameters
- *
- * @param pPars Parameters.
- * @param nVars Inputs.
- * @param nNodes Internal nodes.
- * @param nObjs  Total objects (nVars inputs + nNodes internal nodes).
- * @param nWords The truth table size in 64-bit words.
- * @param iVar The next available SAT variable.
- * @param pTruth: Truth table.
- * @param vInfo: nVars + nNodes + 1.
- * @param VarMarks: Variable marks.
- * @param VarVals: Values of the first nVars variables.
- * @param vOutList: Output vars.
- * @param pSat: SAT solver.
  */
-typedef struct PexaMan_t_ PexaMan_t;
 struct PexaMan_t_ {
+    /// Parameters
     Bmc_EsPar_t * pPars;
+    /// Inputs
     int nVars;
+    /// Internal nodes
     int nNodes;
+    /// Total objects (nVars inputs + nNodes internal nodes)
     int nObjs;
+    /// The truth table size in 64-bit words
     int nWords;
+    /// Start of p variables
+    int iPVariableStart;
+    /// The next available SAT variable
     int iVar;
+    /// Start of minterm variables
     int iVarMintermBase;
+    /// Truth table.
     word * pTruth;
+    /// nVars + nNodes + 1.
     Vec_Wrd_t * vInfo;
+    /// Variable marks.
     int VarMarks[MAJ_NOBJS][2][MAJ_NOBJS];
+    /// Values of the first nVars variables
     int VarVals[MAJ_NOBJS];
+    /// Output vars
     Vec_Wec_t * vOutList;
+    /// SAT solver
     sat_solver * pSat;
 };
 
+typedef struct Comb_t_ Comb_t;
+/**
+ * @brief Combination element.
+ *
+ * @details Combination element storing combinations with their internal data.
+ */
+struct Comb_t_ {
+    /// Switching activity
+    int act;
+    /// Number of nodes
+    int r;
+    /// Combination array
+    int * combi;
+    /// Pointer to next combination element
+    Comb_t * next;
+};
+
+typedef struct CombList_t_ CombList_t;
+/**
+ * @brief Combination priority list.
+ *
+ * @details Combination list storing certain combinations with their internal data.
+ */
+struct CombList_t_ {
+    ///  Pointer to the first combination element in the list.
+    Comb_t * start;
+    ///  Capacity of the list.
+    int len;
+    /// Current length of the list.
+    int length;
+};
+
 int PowerExactSynthesisBase( Bmc_EsPar_t * pPars );
+int PexaManExactPowerSynthesisBasePower( Bmc_EsPar_t * pPars );
 
 #endif
